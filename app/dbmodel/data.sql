6cbad01643d07cdfa832de1b20249886d19b757a
leeheejin0 <jinsis0908@naver.com>
2021-05-12 20:46:54 +0900

    .

---
+++ app/dbmodel/data.sql
@@ -0,0 +1,137 @@
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(1,1,37.50326795334012,127.02430725467384,'비트캠프','7:50:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(1,2,37.51572240443466, 127.01995390348881,'신사역','8:10:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(1,3,37.556133097848345, 126.97240282768777,'서울역','8:30:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(1,4,37.57724467792968, 126.97691877294753, '광화문','8:50:00');
+
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(2,1,37.57724467792968, 126.97691877294753,'광화문','7:50:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(2,2,37.556133097848345, 126.97240282768777,'서울역','8:10:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(2,3,37.51572240443466, 127.01995390348881,'신사역','8:30:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(2,4,37.50326795334012,127.02430725467384,'비트캠프','8:50:00');
+
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(3,1,37.57724467792968, 126.97691877294753,'광화문','7:50:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(3,2,37.53629469418785, 126.9900527343035,'이태원제일교회','8:00:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(3,3,37.50326795334012,127.02430725467384,'비트캠프','8:50:00');
+
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(4,1,37.51572240443466, 127.01995390348881,'신사역','7:10:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(4,2,37.556133097848345, 126.97240282768777,'서울역','8:00:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(4,3,37.48539376190059, 126.90161545473364, '구로디지털단지역','8:50:00');
+
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(5,1,37.499079132390186, 127.02658232085427,'강남역','7:10:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(5,2,37.556133097848345, 126.97240282768777,'서울역','8:00:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(5,3,37.48539376190059, 126.90161545473364, '구로디지털단지역','8:50:00');
+
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(6,1,37.557646052451325, 126.92439179515748,'홍대입구역','7:10:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(6,2,37.51572240443466, 127.01995390348881,'신사역','8:00:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(6,3,37.499079132390186, 127.02658232085427,'강남역','8:50:00');
+
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(7,1,37.56534675340219, 126.9776602419871,'서울광장 분수대','7:50:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(7,2,37.539725486129115, 126.99729116941168,'그랜드하얏트 서울','8:10:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(7,3,37.50326795334012,127.02430725467384,'고속터미널역','8:30:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(7,4,37.499079132390186, 127.02658232085427,'강남역','8:50:00');
+
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(8,1,37.50326795334012,127.02430725467384,'고속터미널역','7:50:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(8,2,37.499079132390186, 127.02658232085427,'강남역','8:30:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(8,3,37.505209315497865, 127.02443464674637, '교보문고 강남점','8:50:00');
+
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(7,1,37.621930705941516, 127.07873319407591,'묵동자이 1단지','7:50:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(7,2,37.60455167428246, 127.07483463748818,'배꽃공원','8:10:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(7,3,37.59019060832125, 127.05798188188558,'회기역','8:10:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(7,4,37.58730007683181, 127.0263040009816,'고려대학교 안암병원','8:30:00');
+
+insert into talat_journey_route(jno,spot_order,lat,lng,spot_name,spot_time) 
+    values(7,5,37.57035455837699, 126.98318626817004,'종각역','8:50:00');
+
+
+
+
+insert into talat_journey(driver,dept,arr,jdate,jtime,seat_psgr_cap,seat_rear_cap,pet,jfee,jcont) 
+    values(2,'비트캠프','광화문','2021-5-12','8:50:00',1,0,1,5000,'장기이용자 여성 1명 있습니다. 괜찮으시면 연락주세요. ')
+
+insert into talat_journey(driver,dept,arr,jdate,jtime,seat_psgr_cap,seat_rear_cap,pet,jfee,jcont) 
+    values(3,'광화문','비트캠프','2021-5-12','8:50:00',1,0,1,5000,'소소한 대화하는 거 좋아합니다. ')
+
+insert into talat_journey(driver,dept,arr,jdate,jtime,seat_psgr_cap,seat_rear_cap,pet,jfee,jcont) 
+    values(4,'광화문','비트캠프','2021-5-12','8:50:00',1,0,1,5000,'장기이용자 여성 1명, 남성 1명 있어요. ')
+
+insert into talat_journey(driver,dept,arr,jdate,jtime,seat_psgr_cap,seat_rear_cap,pet,jfee,jcont) 
+    values(5,'신사역','구로디지털단지역','2021-5-12','8:50:00',1,0,1,5000,'')
+
+insert into talat_journey(driver,dept,arr,jdate,jtime,seat_psgr_cap,seat_rear_cap,pet,jfee,jcont) 
+    values(6,'강남역','구로디지털단지역','2021-5-12','8:50:00',1,0,1,5000,'장기이용자 없음')
+
+insert into talat_journey(driver,dept,arr,jdate,jtime,seat_psgr_cap,seat_rear_cap,pet,jfee,jcont) 
+    values(7,'홍대입구역','강남역','2021-5-12','8:50:00',1,0,1,5000,'장기이용자 없어요. ')
+
+insert into talat_journey(driver,dept,arr,jdate,jtime,seat_psgr_cap,seat_rear_cap,pet,jfee,jcont) 
+    values(8,'서울광장 분수대','강남역','2021-5-12','8:50:00',1,0,1,5000,'반려동물 환영입니다!')
+
+insert into talat_journey(driver,dept,arr,jdate,jtime,seat_psgr_cap,seat_rear_cap,pet,jfee,jcont) 
+    values(9,'고속터미널역','교보문고 강남점','2021-5-12','8:50:00',1,0,1,5000,'')
+    
+insert into talat_journey(driver,dept,arr,jdate,jtime,seat_psgr_cap,seat_rear_cap,pet,jfee,jcont) 
+    values(10,'묵동자이 1단지','종각역','2021-5-12','8:50:00',1,0,1,5000,'')
+
+
+
+insert into talat_memb()
