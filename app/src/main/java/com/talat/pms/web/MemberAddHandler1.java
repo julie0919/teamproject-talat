0e5c81e08a6d045a1132dafd094257a24c8d9c35
hojungyoon <ssuykkif29@gmail.com>
2021-05-17 19:21:58 +0900

    memberAddHandler_2

---
+++ app/src/main/java/com/talat/pms/web/MemberAddHandler1.java
@@ -0,0 +1,22 @@
+package com.talat.pms.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/member/add1")
+public class MemberAddHandler1 extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    response.setContentType("text/html;charset=UTF-8"); 
+    request.getRequestDispatcher("/jsp/member/form1.jsp").include(request, response);
+
+  }
+}
