196517d0072626db9afab8dabcb378eb840e0702
leeheejin0 <jinsis0908@naver.com>
2021-05-18 00:11:54 +0900

    .

--- app/src/main/java/com/talat/pms/web/JourneyAdd1Handler.java
+++ app/src/main/java/com/talat/pms/web/JourneyAdd1Handler.java
@@ -1,7 +1,6 @@
 package com.talat.pms.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,31 +16,7 @@ public class JourneyAdd1Handler extends HttpServlet {
       throws ServletException, IOException {
 
     response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<meta charset='UTF-8'>");
-    out.println("<title>여정 등록</title>");
-    out.println("</head>");
-    out.println("<body>");
-    out.println("<h1>여정 등록: 1단계</h1>");
-    out.println("<p>함께 할 라이더를 찾아볼까요!</p>");
-
-    out.println("<form action='add2' method='post'>");
-    out.println("출발지: <input type='text' name='departure'><br>");
-    //    out.println("경유지1: <input type='text' name='route1'><br>");
-    //    out.println("경유지2: <input type='text' name='route2'><br>");
-    out.println("도착지: <input type='text' name='arrival'><br>");
-    out.println("날짜: <input type='date' name='journeyDate'><br>");
-    out.println("시간: <input type='time' name='journeyTime'><br>");
-    out.println("<p><input type='submit' value='여정 등록하기'>");
-    out.println("<a href='list'>취소</a></p>");
-    out.println("</form>");
-
-    out.println("</body>");
-    out.println("</html>");
+    request.getRequestDispatcher("/jsp/journey/form1.jsp").include(request, response);
   }
 }
 
--- app/src/main/java/com/talat/pms/web/RiderQnAAddHandler.java
+++ app/src/main/java/com/talat/pms/web/RiderQnAAddHandler.java
@@ -1,7 +1,6 @@
 package com.talat.pms.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -9,8 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.talat.pms.domain.Journey;
-import com.talat.pms.domain.Member;
 import com.talat.pms.domain.QnA;
 import com.talat.pms.domain.RiderQnA;
 import com.talat.pms.service.JourneyService;
@@ -38,81 +35,8 @@ public class RiderQnAAddHandler extends HttpServlet {
     JourneyService journeyService = (JourneyService) request.getServletContext().getAttribute("journeyService");
 
     response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
+    request.getRequestDispatcher("/jsp/riderQnA/form.jsp").include(request, response);
 
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<meta charset='UTF-8'>");
-    out.println("<title>새 라이더 문의</title>");
-    out.println("</head>");
-    out.println("<body>");
-    out.println("<h1>새 라이더 문의</h1>");
-
-    try {
-      int no = Integer.parseInt(request.getParameter("no"));
-
-      Journey j = journeyService.get(no);
-      if (j == null) {
-        out.println("<p>해당 번호의 여정이 없습니다.</p>");
-        out.println("</body>");
-        out.println("</html>");
-        return;
-      }
-
-      out.println("<form>");
-      out.println("<table border='1'>");
-      out.println("<tbody>");
-      out.printf("<tr><th>번호</th>"
-          + " <td><input type='text' name='jno' value='%d' readonly></td></tr>\n", j.getJno());
-      out.printf("<tr><th>출발지</th>"
-          + " <td><input name='departure' type='text' value='%s'></td></tr>\n", j.getDeparture());
-
-      out.printf("<tr><th>도착지</th>"
-          + " <td><input name='arrival' type='text' value='%s'></td></tr>\n", j.getArrival());
-      out.printf("<tr><th>여정날짜</th> <td>%s</td></tr>\n", formatterDate.format(j.getJourneyDate()));
-      out.printf("<tr><th>도착시간</th> <td>%s</td></tr>\n", formatterTime.format(j.getJourneyTime()));
-      out.printf("<tr><th>보조석 인원</th> <td>%d명</td></tr>\n", j.getSeatPassenger());
-      out.printf("<tr><th>뒷자석 인원</th> <td>%d명</td></tr>\n", j.getSeatRear());
-
-      if (j.getPet() == 0) {
-        out.println("<tr><th>반려동물 탑승</th>"
-            + " <td>아니오</td></tr>");
-      } else {
-        out.println("<tr><th>반려동물 탑승</th>"
-            + " <td>예</td></tr>");
-      }
-      out.printf("<tr><th>여정 설명</th>"
-          + " <td><textarea name='content' rows='10' cols='60'>%s</textarea></td></tr>\n", j.getContent());
-
-      out.println("</tbody>");
-
-      out.println("</table>");
-    } catch (Exception e) {
-      throw new ServletException(e);    
-    }
-
-    out.println("<form action='add' method='post'>");
-
-    out.println("<hr>");
-    out.println("<select name='qtype'>");
-    out.println("  <option value='0' selected disabled>-- 문의유형 --</option>");
-    out.println("  <option value='1'>분실물 문의</option>");
-    out.println("  <option value='2'>안전문제보고</option>");
-    out.println("  <option value='3'>파트너의 의견 제공</option>");
-    out.println("  <option value='4'>여정 관련 고객지원</option>");
-    out.println("</select>");
-    out.println("첨부파일: <br>");
-
-
-    out.println("<hr>");
-    out.println("문의내용: <textarea name='qContent' rows='10' cols='60'></textarea><br>");
-    out.println("<p><input type='submit' value='등록'>");
-    out.println("<a href='list'>목록</a></p>");
-
-    out.println("</form>");
-    out.println("</body>");
-    out.println("</html>");
   }
 
   @Override
@@ -148,12 +72,8 @@ public class RiderQnAAddHandler extends HttpServlet {
       //            return name + "_30x30";
       //          }
 
-      Member loginUser = (Member) request.getSession().getAttribute("loginUser");
-      rq.setWriter(loginUser);
-
-      riderQnAService.add(rq);
-
-      response.sendRedirect("list");
+      response.setContentType("text/html;charset=UTF-8");
+      request.getRequestDispatcher("/jsp/project/form.jsp").include(request, response);
 
     } catch (Exception e) {
       throw new ServletException(e); 
