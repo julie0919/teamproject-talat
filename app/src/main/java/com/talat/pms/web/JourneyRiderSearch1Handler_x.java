8632191775d8f18a3df361a272e04c8f1e3ac390
cosre7 <cosre7@gmail.com>
2021-05-26 23:39:41 +0900

    journeyRider[Search1Handler추가]

---
+++ app/src/main/java/com/talat/pms/web/JourneyRiderSearch1Handler_x.java
@@ -0,0 +1,23 @@
+package com.talat.pms.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// 여정 검색
+//@SuppressWarnings("serial")
+//@WebServlet("/journey/rider/search1")
+public class JourneyRiderSearch1Handler_x extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    response.setContentType("text/html;charset=UTF-8");
+    request.getRequestDispatcher("/jsp/journeyRider/search1.jsp").include(request, response);
+
+  }
+
+}
