e02ec28116f77d45dfbac42a241e99d625afcd39
hojungyoon <ssuykkif29@gmail.com>
2021-06-03 20:25:05 +0900

    로그아웃 수정

--- app/src/main/java/com/talat/pms/web/LogoutHandler.java
+++ app/src/main/java/com/talat/pms/web/LogoutHandler.java
@@ -1,34 +1,22 @@
 package com.talat.pms.web;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import com.talat.pms.domain.Member;
 
-@SuppressWarnings("serial")
-@WebServlet("/logout")
-public class LogoutHandler extends HttpServlet {
+@Controller
+public class LogoutHandler{
 
-  @Override
-  protected void service(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-
-    response.setContentType("text/plain;charset=UTF-8");
-    PrintWriter out = response.getWriter();
+  @RequestMapping("/logout")
+  public String execute(HttpServletRequest request, HttpServletResponse response)
+      throws Exception {
 
     Member member = (Member) request.getSession().getAttribute("loginUser");
-    if (member == null) {
-      out.println("로그인 하지 않았습니다!");
-      return;
-    }
-
     request.getSession().invalidate();
 
-    out.printf("%s 님 안녕히 가세요!\n", member.getMname());
+    return "redirect:login";
   }
 }
 
